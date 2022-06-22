df91717312b56a9c55fbea59b43ac08550f782a3
gyagoo <ish96116@gmail.com>
2022-06-22 19:20:21 +0900

    [김유정]  - test10 update

--- src/main/java/com/yoojung0318/servlet/test/ServletTest10Controller.java
+++ src/main/java/com/yoojung0318/servlet/test/ServletTest10Controller.java
@@ -29,13 +29,23 @@ public class ServletTest10Controller extends HttpServlet {
 		
 		PrintWriter out = response.getWriter();
 		
+		//id와 password를 전달받고
 		String id = request.getParameter("id");
 		String password = request.getParameter("password");
 		
+		//map에 있는 id password와 파라미터로 전달 받은 id password를 비교해서
+		//안내문구출력
 		out.println("<html> <head><title><h2>자기소개</h2></title></head>");
 		out.println("<body>");
-		out.println("<h1> 로그인 </h1>");
-		
+		//id가 일치하지 않을 때
+		if(!id.equals(userMap.get("id"))) {
+			out.println("<h1>아이디가 일치하지 않습니다.</h1> ");
+		}else  if(password.equals(userMap.get("password"))){	//password가 일치하지 않을때
+			out.println("<h1>비밀번호가 일치하지 않습니다 </h1>");
+		} else {//로그인성공
+			out.println("<h1>" + userMap.get("name") + "님 환영합니다.</h1>");
+		}
+		out.println("</body></html>");
 		
 	}
 
